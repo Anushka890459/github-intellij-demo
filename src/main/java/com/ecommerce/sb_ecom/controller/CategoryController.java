eff9d44a757426a180260b00f11a66c1ed37ca48
Anushka890459 <anushka0909sanjaysangeeta@gmail.com>
2025-04-03 00:43:52 +0530

    commit init

--- src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
@@ -1,17 +1,21 @@
 package com.ecommerce.sb_ecom.controller;
 
 import com.ecommerce.sb_ecom.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 @RestController
 @RequestMapping("api/public")
 public class CategoryController {
-private List<Category> categories = new ArrayList<>();
+
 @GetMapping("categories")
 public List<Category> getAllCategories(){
     return categories;
+}
+@PostMapping("categories")
+public String createCategory(@RequestBody Category category){
+    categories.add(category);
+    return "Category added succesfully";
 }
 }
