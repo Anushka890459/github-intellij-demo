0734685d9d0cb5a107a26b500c998952cb0f0bf7
Anushka890459 <anushka0909sanjaysangeeta@gmail.com>
2025-04-03 21:59:13 +0530

    commit init

--- src/main/java/com/ecommerce/sb_ecom/model/Category.java
+++ src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -3,4 +3,20 @@ package com.ecommerce.sb_ecom.model;
 public class Category {
     private long categoryId;
     private String categoryName;
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
 }
