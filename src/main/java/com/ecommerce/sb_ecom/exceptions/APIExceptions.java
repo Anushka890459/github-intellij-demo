c217ba96b966a6a81bc138285d77f0ba4c98a739
Anushka890459 <anushka0909sanjaysangeeta@gmail.com>
2025-06-25 12:36:09 +0530

    commit init

---
+++ src/main/java/com/ecommerce/sb_ecom/exceptions/APIExceptions.java
@@ -0,0 +1,12 @@
+package com.ecommerce.sb_ecom.exceptions;
+
+public class APIExceptions extends RuntimeException{
+   private static final long serialVersionUID = 1L;
+
+    public APIExceptions() {
+    }
+
+    public APIExceptions(String message) {
+        super(message);
+    }
+}
